7bc2a067447332eba11f9a167ec48263b445e7a5
Jr Bytes <itamazor@gmail.com>
2020-08-03 16:54:40 -0300

    :tada: Initial commit.

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Landing() {
+  return (
+    <div id="page-landing">
+      <div id="page-landing-content" className="container">
+        <div className="logo-container">
+
+        </div>
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Landing
